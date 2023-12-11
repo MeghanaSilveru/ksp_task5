6cef5bb039d16ecf056546a70a6ac5f77861a366
Meghana Silveru <153207488+MeghanaSilveru@users.noreply.github.com>
2023-12-11 22:34:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+alert("NOTIFICATION");
+console.log("NICE MEETING YOU");// NICE MEETING YOU
+let A=10;
+let B=20;
+let C=30;
+const D=40;
+console.log(C); //30
+
+let name="meghana"; //string type
+let age=21;// integer type
+let good=true;//boolean type
+console.log(name);// meghana
+console.log(age);// 21
+console.log(good);// true
+ A=50;
+console.log(A); // 50
+console.log(D);// 40
+result= (A-B)*2;
+console.log(result); // 60
+
+// ARITHMETIC OPERATORS LIKE + - * / % ** ++ --
+console.log(A+B);// 70
+console.log(B-C);// -10
+console.log(B*C);// 600
+console.log(D/B);// 2
+console.log(D%B);// 0
+console.log(C**B);// 3.486784401E29
+console.log(A++);// 50
+B++;
+console.log(B);// 21
+console.log(C--);// 30
+age--;
+console.log(age);// 20
+
+// assignment operator like += -= *= /= %= **
+let number1=2;
+let number2=3;
+let number3=4;
+let number4=5;
+result1= number1+10;
+console.log(result1);// 12
+console.log(number1+=10);// 12 
+console.log(number2-=20);// -17
+console.log(number3*=10);// 40
+console.log(number4**=3);// 125
+
+//comparision operator
+let number5=2;
+let number6="2";
+let number7=2;
+let number8=2;
+console.log(number5=50);// 50
+console.log(number7!=number5);// true
+console.log(number5==number6);// false
+console.log(number7===number6);// false
+console.log(number7==number8);// true
+console.log(number7===number8);// true
+console.log(number5<number8);// false
+console.log(number7<=number8);// true
+console.log(number5>number8);// true
+console.log(number7>=number8);//true
+
+
+
+
+
+
